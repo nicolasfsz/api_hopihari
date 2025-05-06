f45c426e5cc00009ca312954fd5b0b036fd2d0a8
nicolasfsz <nicolas.desouza02062008@gmail.com>
2025-05-06 11:27:28 -0300

    terceiro commit

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -32,9 +32,10 @@ exports.atualizarUsuario = async (req, res,) => {
 
 exports.cadastrarUsuario = async (req, res) => {
     try {
+        const hash = await bcrypt.hash(req.body.password, 10);
         const resultado = await mysql.execute(
             `insert into users (first_name, last_name, email, password, birth_date, phone) values (?, ?, ?, ?, ?, ?);`,
-            [req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.birth_date, req.body.phone]
+            [req.body.first_name, req.body.last_name, req.body.email, hash, req.body.birth_date, req.body.phone]
         );
         return res.status(201).send({ 
             "mensagem": "Usuario cadastrado com sucesso!",
@@ -67,8 +68,8 @@ exports.deletarUsuario = async (req, res) => {
 exports.login = async (req, res) => {
     try {
         const usuario = await mysql.execute(
-            `SELECT * FROM users WHERE email = ? and password = ?;`,
-            [req.body.email, req.body.password]
+            `SELECT * FROM users WHERE email = ?;`,
+            [req.body.email]
         );
         if (usuario.length == 0) {
             return res.status(401).send({ 
@@ -76,13 +77,14 @@ exports.login = async (req, res) => {
             });
         }
 
-        const match = await bcrypt.compare(usuario[0].password, req.body.password)
+        const match = await bcrypt.compare (req.body.password, usuario[0].password)
+
         if (!match) {
             return res.status(401).send({ 
                 "mensagem": "senha incorreta!"
             });
         }
-        console.log(match, req.body.password, usuario[0].password);
+       
 
         const token = jwt.sign({
             id: usuario[0].id,
@@ -94,7 +96,7 @@ exports.login = async (req, res) => {
         }, 'senhajwt');
         return res.status(201).send({ 
             "mensagem": "Usuario logado com sucesso!",
-            "resultado": resultado
+            "token": token
              });
     }
     catch (error) {
